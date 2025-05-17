d4e8219c4c13a9735137f80875867223de86477b
biancalustosa <biafelix08@gmail.com>
2025-05-16 21:47:21 -0300

    Adição da funcionalidade dos botões editar e excluir da tabela Minhas Avaliaões

---
+++ Clube Mangaque/hq/editarAvaliacao.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+    require_once 'conexaoBD.php';
+
+    /*
+        if (!isset($_SESSION['IdUsuario'])) {
+        http_response_code(401);
+        echo json_encode(['erro' => 'Usuário não autenticado']);
+        exit;
+    }
+    */
+    
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    $id = $data['idavaliacao'];
+    $comentario = $conn->real_escape_string($data['comentarioEditar']);
+    $nota = intval($data['notaEditar']);
+
+    $sql = "UPDATE avaliacoes SET comentario='$comentario', nota=$nota WHERE idavaliacao=$id";
+
+    if($conn->query($sql)){
+        echo json_encode(['sucesso'=>true]);
+    }else{
+        echo json_encode(['sucesso'=> false, 'erro' => $conn->error]);
+    }
+    
+?>
